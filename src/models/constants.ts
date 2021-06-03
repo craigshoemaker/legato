0cbd6e4a4b13c9e317c877f0fb95ae7f9ed7caf0
John Papa <john@johnpapa.net>
2021-06-02 23:10:35 -0400

    reduce to smallerset of colors, that work in light or dark

--- src/models/constants.ts
+++ src/models/constants.ts
@@ -8,12 +8,10 @@ export const extensionId = 'craigshoemaker.vscode-legato';
 export const isObjectEmpty = (o: {} | undefined) => typeof o === 'object' && Object.keys(o).length === 0;
 
 export const colors = [
-  { name: 'Angular Red', value: '#dd0531' },
-  { name: 'Azure Blue', value: '#007fff' },
-  { name: 'JavaScript Yellow', value: '#f9e64f' },
-  { name: 'Node Green', value: '#215732' },
   { name: 'React Blue', value: '#61dafb' },
-  { name: 'Something Different', value: '#832561' },
+  { name: 'JavaScript Yellow', value: '#f9e64f' },
   { name: 'Svelte Orange', value: '#ff3d00' },
   { name: 'Vue Green', value: '#42b883' },
+  { name: 'Azure Blue', value: '#007fff' },
+  { name: 'Something Different', value: '#832561' },
 ];
