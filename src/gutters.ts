84533194eed7ac1b9bd72c687ea34c6612e6fc64
John Papa <john@johnpapa.net>
2021-06-02 23:21:07 -0400

    disposing of the scopes so the gutters will be freshly decorated every time we need them

--- src/gutters.ts
+++ src/gutters.ts
@@ -1,22 +1,35 @@
 import * as vscode from 'vscode';
 import { Uri } from 'vscode';
+import { Logger } from './logging';
 import { Area, colors } from './models';
 
 let { activeTextEditor } = vscode.window;
 
+let scopeDecorations: vscode.TextEditorDecorationType[] = [];
+
+/**
+ * @description Find the matches for the tokens. Create a range using the line numbers. Then decorate the range using the pre-defined colors.
+ */
 export function updateDecorations() {
+  disposeScopeDecorations();
+
   if (!activeTextEditor) {
     return;
   }
+
+  Logger.info('Setting Gutters');
+
   const regEx = /# \[(.*)\]|---/gi;
   const text = activeTextEditor.document.getText();
-  const areas: Area[] = [];
+  let areas: Area[] = [];
   let match;
+
   let skipper = 0;
   while ((match = regEx.exec(text))) {
     /**
+     * TODO: This is a hack.
      * We skip the first 2 matches,
-     * because our regEx isnt yet excluding
+     * because our regEx isn't yet excluding
      * the top metadata material
      **/
     skipper++;
@@ -24,37 +37,82 @@ export function updateDecorations() {
       continue;
     }
 
-    /**
-     * Find the start and end positions where we match the regEx for the tab area.
-     */
-    const startPos = activeTextEditor.document.positionAt(match.index);
-    const endPos = activeTextEditor.document.positionAt(match.index + match[0].length);
+    // Find the start and end positions where we match the regEx for the tab area.
+    const { positionAt } = activeTextEditor.document;
+    const startPos = positionAt(match.index);
+    const endPos = positionAt(match.index + match[0].length);
 
-    /**
-     * Create the deco options using the range.
-     */
+    // Create the deco options using the range.
     const decorationOptions = {
       range: new vscode.Range(startPos, endPos),
       hoverMessage: match[0],
     };
 
-    /**
-     * Set the color for the gutterIcon to rotate through our color constants.
-     */
+    // Set the color for the gutterIcon to rotate through our color constants.
     const index = areas.length % colors.length;
     const decorationType = vscode.window.createTextEditorDecorationType({
       gutterIconPath: createIcon(colors[index].value),
+      gutterIconSize: 'auto',
     });
 
-    /**
-     * Add the area to be colored in the gutters.
-     */
     areas.push({ decorationOptions, decorationType });
   }
 
-  /**
-   * Modify the areas to cover the entire area.
-   */
+  areas = extendAreaToCoverEntireRange(areas);
+
+  applyGutters(areas);
+}
+
+/**
+ * @description Sets the decorations to the gutter.
+ * @param areas The areas (ranges and decorations) to apply to the gutters
+ */
+function applyGutters(areas: Area[]) {
+  areas.forEach((area) => {
+    scopeDecorations.push(area.decorationType);
+    activeTextEditor?.setDecorations(area.decorationType, [area.decorationOptions]);
+  });
+}
+
+/**
+ * @description Create the gutter icon.
+ * @param color Icon color
+ * @returns The Uri for the SVG icon
+ */
+function createIcon(color: string): Uri {
+  const lineHeight = vscode.workspace.getConfiguration('editor').lineHeight;
+  const offset = 10;
+  const width = 3;
+  const transparency = 99;
+
+  const svg =
+    `<svg xmlns="http://www.w3.org/2000/svg" height="${lineHeight}" width="${lineHeight}">` +
+    `<rect x="${offset}" y="0" width="${width}" height="100" style="fill: ${color}${transparency};"></rect>;` +
+    `</svg>`;
+
+  const encodedSVG = encodeURIComponent(svg);
+  const URI = 'data:image/svg+xml;utf8,' + encodedSVG;
+  return Uri.parse(URI);
+}
+
+/**
+ * Remove all scope decorations.
+ * This is required so we can constantly update the gutter.
+ */
+function disposeScopeDecorations() {
+  for (const decoration of scopeDecorations) {
+    decoration.dispose();
+  }
+
+  scopeDecorations = [];
+}
+
+/**
+ * @description Extend the ranges of each match in the array to the next match. This is what decorates the entire range in the gutter.
+ * @param areas array of the styles and ranges for the gutter
+ * @returns a fresh copy of the areas array
+ */
+function extendAreaToCoverEntireRange(areas: Area[]) {
   let previousArea: Area;
   areas.forEach((area) => {
     const { line } = area.decorationOptions.range.start;
@@ -66,24 +124,5 @@ export function updateDecorations() {
 
     previousArea = area;
   });
-
-  /**
-   * Apply the gutters
-   */
-  areas.forEach((area) => {
-    activeTextEditor?.setDecorations(area.decorationType, [area.decorationOptions]);
-  });
-}
-
-function createIcon(color: string): Uri {
-  const lineHeight = vscode.workspace.getConfiguration('editor').lineHeight;
-
-  const svg =
-    `<svg xmlns="http://www.w3.org/2000/svg" height="${lineHeight}" width="${lineHeight}">` +
-    `<rect x="0" y="0" width="10" height="30" style="fill: ${color};"></rect>;` +
-    `</svg>`;
-
-  const encodedSVG = encodeURIComponent(svg);
-  const URI = 'data:image/svg+xml;utf8,' + encodedSVG;
-  return Uri.parse(URI);
+  return areas;
 }
