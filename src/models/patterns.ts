099408671ead6f74804c5f886b6b2a1d7c5854c5
craigshoemaker <craig@craigshoemaker.net>
2021-06-04 15:19:50 -0400

    document the tabs regex

--- src/models/patterns.ts
+++ src/models/patterns.ts
@@ -4,8 +4,37 @@ import { AreaPattern, AreaPatternTypes } from './interfaces';
 
 export const patternTypes: AreaPatternTypes = {
   tabs: {
+    /*
+      A two part expression, separated by the OR (|) operator.
+            
+      == PART 1: ==== /# \[(.*)\] ====================
+        Match all the tab definitions and captures
+        the tab labels for display.
+      ================================================
+      #             a sigle pound sign and single space
+      \[            open square bracket
+      (.*)          unlimited series of non-line break charaters in a caputre group (the tab label)
+      \]            close square bracket
+
+      == PART 2: ==== | ==============================
+        The OR operator
+      ================================================
+
+      |             OR operator
+
+      == PART 3: ==== (?<=(# [.\S\s]*))---[\n\r] ====
+        Match "---", but skip metadata delimiters by
+        only matching when "---" is preceded by "#".
+      ================================================
+      (?<=          open up a look behind expression
+      (# [.\S\s]*)  require that "#" plus any other text is present
+      )             close the look behind expression
+      ---           match on three dashes
+      [\n\r]        match on line break (\n) and carriage return (\r) -> necessary to ensure the pattern does not match on table definitions
+
+      flags:        global and case insensitive
+    */
     regex: /# \[(.*)\]|(?<=(# [.\S\s]*))---[\n\r]/gi,
-    // regex: /# \[(.*)\]|(?<=#)---[\n\r]/gi,
     name: SwitcherTypes.tabs,
   },
   zones: {
